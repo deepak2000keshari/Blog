5db4b3909f7c7523b2dbe0be2378d1bb2ac2fd68
deepak keshari <deepakbaburi2000@gmail.com>
2025-05-04 00:43:30 +0530

    do store in small due to import in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { store } from './APP/Store.js';
+import { store } from './APP/store.js';
 import { Provider } from 'react-redux';
 import './index.css';
 import { BrowserRouter } from "react-router";
